98a21d818a572fd84150bdb036f34f59b3e3b1ac
Kitamura <ymyk6602@gmail.com>
2022-07-18 16:57:48 +0900

    add gatsby tutorial

---
+++ my-gatsby-site/src/components/layout.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+
+type props = {
+  pageTitle: string;
+  children: React.ReactNode;
+};
+
+const Layout = ({ pageTitle, children }: props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <div>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ my-gatsby-site/src/pages/about.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About me">
+      <p>
+        Hi there! I'm the proud creator of this site, when I built with Gatsby
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
